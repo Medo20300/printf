d2df26360265ddab45de0fa66777a5dadd4b8d06
Medo20300 <medhatconcrete@gmail.com>
2023-10-15 16:30:14 -0700

    part two

--- main.h
+++ main.h
@@ -21,5 +21,37 @@ int print_percent(int parames, ...);
 
 void print_int_formatter(int num, char *str);
 
+int _printf(const char *, ...);
+
+/*** print_handle_arguments.c****/
+int handle(const char *, va_list);
+int percent_handle(const char *, va_list, int *);
+
+
+/*** printer **/
+
+int print_string(va_list);
+int print_integer(va_list);
+int print_rot(va_list);
+int print_octal(va_list);
+int print_hexadecimal_upp(va_list);
+int print_rev_string(va_list);
+int print_char(va_list);
+int print_binary(va_list);
+int print_unsigned(va_list);
+int print_hexadecimal_low(va_list);
+int print_pointer(va_list);
+
+/**
+ * struct_formats- struct
+ * @type: struct formatss
+ * @f: print formats
+ */
+typedef struct _format
+{
+	char type;
+	int (*f)(va_list);
+} format;
+
 
 #endif /* MAIN_H */

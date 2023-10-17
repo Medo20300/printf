6227cf996cda2f98f202bbc4ab1c890fef3b7200
MostafaAlaa297 <elaskary609@gmail.com>
2023-10-17 11:01:26 -0700

    Update main.h

--- main.h
+++ main.h
@@ -1,59 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdarg.h>
-#include <unistd.h>
-#include <string.h>
-#include <math.h>
-#include <limits.h>
-/** flags  ***/
+int print_char(char c);
 
-#define F_MINUS 1
+int print_int_formatter(int num);
 
+int _printf_custom(const char *, ...);
 
-/*** prototypes for the print function ***/
+int _puts(cahr *str);
 
-int print_char(char buffer[], int flags, int width, int precision,
-		int size);
-
-int print_string(va_list arguments, char buffer[], int flags, int width, int precision,
-		int size);
-int print_percent(int parames, ...);
-
-void print_int_formatter(int num, char *str);
-
-int _printf(const char *, ...);
-
-/*** print_handle_arguments.c****/
-int handle(const char *, va_list);
-int percent_handle(const char *, va_list, int *);
-
-
-/*** printer **/
-
-int print_string(va_list);
-int print_integer(va_list);
-int print_rot(va_list);
-int print_octal(va_list);
-int print_hexadecimal_upp(va_list);
-int print_rev_string(va_list);
-int print_char(va_list);
-int print_binary(va_list);
-int print_unsigned(va_list);
-int print_hexadecimal_low(va_list);
-int print_pointer(va_list);
-
-/**
- * struct_formats- struct
- * @type: struct formatss
- * @f: print formats
- */
-typedef struct _format
-{
-	char type;
-	int (*f)(va_list);
-} format;
-
-
+int _putchar(char c);
+	
 #endif /* MAIN_H */

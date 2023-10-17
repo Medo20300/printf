cac1bc18b9c254e042a05d7dd0061ceb6b6ddd89
MostafaAlaa297 <elaskary609@gmail.com>
2023-10-17 12:28:00 -0700

    Update main.h and _printf

--- main.h
+++ main.h
@@ -3,48 +3,11 @@
 
 int print_char(char c);
 
-<<<<<<< HEAD
-#define F_MINUS 1
-#define F_PLUSE 2
-#define F_ZERO 4
-#define F_HASH 8
-#define F_SPACE 16
-
-/**Size **/ 
-#define S_LONG 2
-#define S_SHORT 1
-
-/*** prototypes for the print function ***/
-
-int print_char(char ch);
-
-int print_string(va_list arguments);
-
-int print_percent(int parames, ...);
-
 int print_int_formatter(int num);
 
 int _printf(const char *, ...);
 
-int _printf_custom(const char *format, ...);
-/**
- * struct_formats- struct
- * @type: struct formatss
- * @f: print formats
- */
-typedef struct _format
-{
-	char type;
-	int (*f)(va_list);
-} format;
-
-=======
-int print_int_formatter(int num);
-
-int _printf_custom(const char *, ...);
-
 int _puts(cahr *str);
->>>>>>> 6227cf996cda2f98f202bbc4ab1c890fef3b7200
 
 int _putchar(char c);
 	
